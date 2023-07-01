d5eb091d38205a43a9f45a866edba6c86a0d7d55
Justin <contact@justinpitts.dev>
2023-07-01 19:26:49 -0400

    Show prompt from chatgpt

--- lib/app.rb
+++ lib/app.rb
@@ -1,6 +1,14 @@
 require 'dotenv/load'
 require 'sinatra'
+require 'openai'
 
 get '/' do
-  'Hello world!'
+  client = OpenAI::Client.new(access_token: ENV['OPENAI_ACCESS_TOKEN'])
+
+  response = client.chat(
+    parameters: {
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: "Hello!"}]
+    })
+  response.dig("choices", 0, "message", "content")
 end
