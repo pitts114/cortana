db7d6b11cfcad4a01375c5ab84e1591d47c3482d
Justin <contact@justinpitts.dev>
2023-07-01 21:39:35 -0400

    Use prompt from request

--- lib/app.rb
+++ lib/app.rb
@@ -2,13 +2,17 @@ require 'dotenv/load'
 require 'sinatra'
 require 'openai'
 
-get '/' do
+post '/' do
   client = OpenAI::Client.new(access_token: ENV['OPENAI_ACCESS_TOKEN'])
+
+  request.body.rewind
+  request_payload = JSON.parse request.body.read
+  prompt = request_payload["prompt"] || raise("Request provide a prompt")
 
   response = client.chat(
     parameters: {
         model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: "Hello!"}]
+        messages: [{ role: "user", content: prompt}]
     })
   response.dig("choices", 0, "message", "content")
 end
