1b63870f8c9ff9100a4d6ba5a2a37dc7cd703971
Justin <contact@justinpitts.dev>
2023-07-01 22:01:44 -0400

    Return tts audio of chatgpt response

--- lib/app.rb
+++ lib/app.rb
@@ -18,5 +18,10 @@ post '/' do
         model: "gpt-3.5-turbo",
         messages: [{ role: "user", content: prompt}]
     })
-  response.dig("choices", 0, "message", "content")
+  content = response.dig("choices", 0, "message", "content")
+
+  response = Net::HTTP.post(URI("http://#{ENV['MIMIC3_HOST']}:#{ENV['MIMIC3_PORT']}/api/tts"), content)
+  wav = response.body
+
+  wav
 end
